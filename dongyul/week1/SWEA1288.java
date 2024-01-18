bd8a47797184bd701f79612f885142cef8b1a76d
이동열(Lee Dong Yul) <comeme0101@naver.com>
2024-01-18 16:02:45 +0900

    Update SWEA1288.java
    [Success] dongyul/Bit-wise Oper/SWEA1288.java/ 배열 사용 -> 비트연산으로 코드 변경

--- dongyul/week1/SWEA1288.java
+++ dongyul/week1/SWEA1288.java
@@ -6,43 +6,34 @@ import java.io.InputStreamReader;
 
 public class SWEA1288 {
 
-    public static Boolean all(int[] arr) {
-        Boolean flag = true;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                flag = false;
-                break;
-            }
-        }
-        return flag;
-    }
-
     public static int sol(int num) {
-        int m = num;
-        int[] arr = new int[10];
-        while (true) {
-            int temp = num;
-            while (temp > 0) {
-                int idx = temp % 10;
-                arr[idx] += 1;
-                temp /= 10;
-            }
-            if (all(arr)) {
-                break;
-            }
-            num += m;
-        }
-        return num;
-    }
+		int m = num;
+		int bitmap = 0;
+		while (true) {
+			int temp = num;
+			while (temp > 0) {
+				int idx = temp % 10;
+				int bit = 1<<idx;
+				bitmap = bitmap|bit;
+				temp /= 10;
+			}
+			if (Integer.bitCount(bitmap) == 10) {
+				break;
+			}
+			num += m;
+		}
+		return num;
+	}
+	
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        for (int i = 0; i < T; i++) {
-            StringBuilder sb = new StringBuilder();
-            int num = Integer.parseInt(br.readLine());
-            sb.append("#").append(i+1).append(" ").append(sol(num));
-            System.out.println(sb.toString());
-        }
-    }
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int i = 0; i < T; i++) {
+			StringBuilder sb = new StringBuilder();
+			int num = Integer.parseInt(br.readLine());
+			sb.append("#").append(i+1).append(" ").append(sol(num));
+			System.out.println(sb.toString());
+		}
+	}
 }
