4fd1c75ffb596afd7575e32a1edf11256319d118
Youngseon Seo <70623206+youngseonseo@users.noreply.github.com>
2024-01-23 19:39:08 +0900

    [success] SWEA_1288_새로운 불면증 치료법| 비트 연산자

--- youngseon/week1/SWEA_1288.java
+++ youngseon/week1/SWEA_1288.java
@@ -1,7 +1,18 @@
 import java.io.*;
-import java.util.Arrays;
+import java.util.*;
 
 public class Solution {
+	
+	private static boolean containsFalse(boolean[] array) {
+	    for (boolean value : array) {
+	        if (!value) {
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	
+	
 	public static void main(String[] args) throws IOException {
 	
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,26 +20,30 @@ public class Solution {
 		
 		int N;
 		for (int i = 0; i < T; i++) {
-			int m = 1;     // 곱하는 수
-			int check[] = new int[10];          // 0 ~ 9 숫자 나왔는지 체크 -> 없으면 0, 있으면 1
+			int m = 1;     // 곱하는 수   
+		
 			N = Integer.parseInt(br.readLine());
+			boolean[] check = new boolean[10];
+
+		
+//			while (Arrays.asList(check).contains(false)) {	  // 이거 왜 안됨??
+//			while (Arrays.asList(check).stream().filter(c -> c == true).count()<10) {   // 얘는 또 왜 안됨??
 			
-			while (Arrays.stream(check).sum() <10 ) {
-				
-				
+			while(containsFalse(check)) {
+				boolean[] now_check = new boolean[10];
 				char[] arr = String.valueOf(N*m).toCharArray(); 
-			
-				for (int j = 0; j < arr.length; j++) {
-					
-					int tf = check[arr[j]-'0'];   // 이미 나왔는지 확인 
-					if(tf == 0) {
-						check[arr[j]-'0']=1;    // 안나왔다면 1로 변경
-					}
+				for (char ch : arr) {
+					int idx = ch - '0';   
+					now_check[idx] = true;
 				}
+				for(int j=0; j<10; j++) {
+					check[j] = now_check[j] | check[j];     // 비트 연산(or)으로 체크
+				}
+				
+				
 				m++;
 			}
 			System.out.println("#" + (i+1) + " "+ (N*(m-1)));
 		}
 	}
-
 }
