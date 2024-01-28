696c9a81fb092725f44f0fea82d92cf75417038b
강혁준 <105738124+kanghyukjun@users.noreply.github.com>
2024-01-28 11:51:59 +0900

    [fail] Boj_22856_트리 순회 | ArrayList + 중위 순회

--- hyukjun/week2/boj_22856_트리순회.java
+++ hyukjun/week2/boj_22856_트리순회.java
@@ -1,8 +1,8 @@
-package baekjoon;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Boj_22856 {
@@ -19,8 +19,9 @@ public class Boj_22856 {
 
 	static int N;
 	static Node[] list;
-	
-	static int check = 0;
+	static int lastIdx = -1;
+	static List<Integer> inOrder;
+
 	static int count = 0;
 
 	public static void main(String[] args) throws IOException {
@@ -32,46 +33,61 @@ public class Boj_22856 {
 		for (int i = 1; i <= N; i++) {
 			list[i] = new Node(i);
 		}
-		
+
 		StringTokenizer st;
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int idx = Integer.parseInt(st.nextToken());
 			int left = Integer.parseInt(st.nextToken());
 			int right = Integer.parseInt(st.nextToken());
-			
-			if(left != -1) {
+
+			if (left != -1) {
 				list[idx].left = list[left];
 			}
-			if(right != -1) {
+			if (right != -1) {
 				list[idx].right = list[right];
 			}
 		}
 
 		// process
+		inOrder = new ArrayList<>();
+		findLast(list[1]);
+		lastIdx = inOrder.get(inOrder.size() - 1);
+		
 		solve(list[1]);
 	}
 
-	private static void solve(Node node) {
+	private static void findLast(Node node) {
 		// 왼쪽 노드 순회
-		if(node.left != null && (check & (1 << node.left.value)) == 0) {
+		if (node.left != null) {
+			findLast(node.left);
+		}
+
+		// 현재 노드 방문 처리
+		inOrder.add(node.value);
+
+		// 오른쪽 노드 순회
+		if (node.right != null) {
+			findLast(node.right);
+		}
+	}
+
+	private static void solve(Node node) {
+		if (node.left != null) {
+			// 왼쪽 노드 순회
 			count++;
 			solve(node.left);
 			count++;
 		}
-		
-		// 현재 노드 방문 처리
-		// 만약 모두 방문했으면 종료
-		check |= (1 << node.value);
-		if(check == (((1 << (N + 1)) - 2))) {
-			System.out.println(count);
-		}
-		
-		// 오른쪽 노드 순회
-		if(node.right != null && (check & (1 << node.right.value)) == 0) {
+		if (node.right != null) {
+			// 오른쪽 노드 순회
 			count++;
 			solve(node.right);
 			count++;
+		}
+		if (node.value == lastIdx) {
+			// 만약 모두 방문했으면 종료
+			System.out.println(count);
 		}
 	}
 
