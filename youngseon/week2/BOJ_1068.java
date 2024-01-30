d181dff3ef2b4d2b1a713415b32da39b560e0a98
Youngseon Seo <70623206+youngseonseo@users.noreply.github.com>
2024-01-30 11:50:09 +0900

    [success] BOJ_1068_트리 | DFS + ArrayList

--- youngseon/week2/BOJ_1068.java
+++ youngseon/week2/BOJ_1068.java
@@ -4,53 +4,23 @@ import java.util.*;
 public class BOJ_1068 {
 
 	private static int N;
-	private static int count; // 리프노드 개수
+	private static int count = 0; // 리프노드 개수
+	private static int remove = 0; // 삭제할 노드
 	private static ArrayList<Integer>[] tree;
 	private static boolean visited[];
 
-	private static void printCount() { // 단말 노드 출력 로직
-		/*
-		 * 
-		 * [1] [3, 4] [] [] []
-		 */
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < tree[i].size(); j++) {
-				int num = tree[i].get(j);
-				if (tree[num].size() == 0) {
-					count++;
-				}
-			}
-		}
-		System.out.println(count);
-
-	}
-
-	private static int find_where(int n) {
-		for (int i = 0; i < N; i++) {    // 해당 숫자가 있는 배열의 위치 반환 로직
-			if (tree[i].contains(n)) {
-				return i;
-			}
-		}
-		return -1;
-	}
-
-	private static void dfs(int remove) { // 삭제 로직
-		if (visited[remove]) {
-			return;
-		}
-		visited[remove] = true;
-		int idx = find_where(remove);
-		if(idx !=-1) {
-			tree[idx].remove(Integer.valueOf(remove));
-		}
-		ArrayList<Integer> copy = tree[remove];
-		tree[remove] = new ArrayList<>();
-		for (int i : copy) {
-			if (visited[i] == false) {
+	private static void dfs(int num) { // 삭제 로직
+		visited[num] = true;
+		int cNode = 0;
+		for (int i : tree[num]) {
+			if (visited[i] == false && i != remove) {
+				cNode++;
 				dfs(i);
 			}
 		}
-		
+		if (cNode == 0) {
+			count++;
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -62,23 +32,27 @@ public class BOJ_1068 {
 		tree = new ArrayList[N]; // ArrayList로 트리 구현
 		visited = new boolean[N];
 
+		int root = 0;
 		for (int i = 0; i < N; i++) {
-			tree[i] = new ArrayList<>();
+			tree[i] = new ArrayList<Integer>();
 		}
 
 		for (int i = 0; i < N; i++) {
 			int parent_index = Integer.parseInt(st.nextToken());
 			if (parent_index != -1) {
 				tree[parent_index].add(i); // 값 저장
+				tree[i].add(parent_index);
+			} else {
+				root = i;
 			}
 		}
 
-		int remove = Integer.parseInt(br.readLine()); // 삭제할 노드 찾고, 자식 노드 없애기
-		if (remove == 0) { // 삭제 노드가 0이면 루트 노드만 남으므로
+		remove = Integer.parseInt(br.readLine()); // 삭제할 노드 찾고, 자식 노드 없애기
+		if (remove == root) { // 삭제 노드 부모가 루트 노드이면
 			System.out.println(0);
 		} else {
-			dfs(remove);
-			printCount();
+			dfs(root);
+			System.out.println(count);
 		}
 //		for(int i=0;i<N;i++) {
 //			System.out.println(tree[i]);
