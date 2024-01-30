0163e1f35965f999b5a03e4411901b697fd64543
Youngseon Seo <70623206+youngseonseo@users.noreply.github.com>
2024-01-30 09:44:02 +0900

    [fail] BOJ_1068_트리 | ArrayList + DFS

--- youngseon/week2/BOJ_1068.java
+++ youngseon/week2/BOJ_1068.java
@@ -1,11 +1,12 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ1068 {
+public class BOJ_1068 {
 
 	private static int N;
 	private static int count; // 리프노드 개수
 	private static ArrayList<Integer>[] tree;
+	private static boolean visited[];
 
 	private static void printCount() { // 단말 노드 출력 로직
 		/*
@@ -24,27 +25,32 @@ public class BOJ1068 {
 
 	}
 
-	private static void check(int remove) { // 삭제 로직
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < tree[i].size(); j++) {
-				if (tree[i].get(j) == remove) {
-					tree[i].remove(j); // 해당 노드 삭제
-					break;
-				}
+	private static int find_where(int n) {
+		for (int i = 0; i < N; i++) {    // 해당 숫자가 있는 배열의 위치 반환 로직
+			if (tree[i].contains(n)) {
+				return i;
 			}
 		}
-		while(true) {  // 모든 자손 노드 없애기
-			ArrayList<Integer> list = tree[remove];
-			tree[remove] = new ArrayList<>(); 
-			
-			if(list.size()==0) {
-				break;
-			}
-			for(int k: list) {
-				check(k);
+		return -1;
+	}
+
+	private static void dfs(int remove) { // 삭제 로직
+		if (visited[remove]) {
+			return;
+		}
+		visited[remove] = true;
+		int idx = find_where(remove);
+		if(idx !=-1) {
+			tree[idx].remove(Integer.valueOf(remove));
+		}
+		ArrayList<Integer> copy = tree[remove];
+		tree[remove] = new ArrayList<>();
+		for (int i : copy) {
+			if (visited[i] == false) {
+				dfs(i);
 			}
 		}
-	
+		
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -54,6 +60,8 @@ public class BOJ1068 {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 
 		tree = new ArrayList[N]; // ArrayList로 트리 구현
+		visited = new boolean[N];
+
 		for (int i = 0; i < N; i++) {
 			tree[i] = new ArrayList<>();
 		}
@@ -69,8 +77,11 @@ public class BOJ1068 {
 		if (remove == 0) { // 삭제 노드가 0이면 루트 노드만 남으므로
 			System.out.println(0);
 		} else {
-			check(remove);
+			dfs(remove);
 			printCount();
 		}
+//		for(int i=0;i<N;i++) {
+//			System.out.println(tree[i]);
+//		}
 	}
 }
