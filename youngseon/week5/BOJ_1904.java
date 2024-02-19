9c49d2ed70845be2c367eb341ad504266af90d95
Youngseon Seo <70623206+youngseonseo@users.noreply.github.com>
2024-02-19 17:34:08 +0900

    [success] BOJ_1904_01타일/ DP

---
+++ youngseon/week5/BOJ_1904.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class BOJ_1904 {
+
+	static int[] DP;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		DP = new int[1000001];
+
+		DP[0] = 0;
+		DP[1] = 1;
+		DP[2] = 2;
+
+		for (int i = 3; i <= N; i++) {
+			DP[i] = (DP[i - 2] + DP[i - 1]) % 15746;
+
+		}
+
+		System.out.println(DP[N]% 15746);
+	}
+}
