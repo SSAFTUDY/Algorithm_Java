759686d1e07ce2e5e7e9f9b8e522d766ac619313
rlawnsdn <junoo1997@naver.com>
2024-02-28 17:24:51 +0900

    [SUCCESS]BOJ_10427_빚|정렬후계산

--- junwoo/week6/BOJ10427_빚.java
+++ junwoo/week6/BOJ10427_빚.java
@@ -1,47 +1,45 @@
 import java.io.*;
 import java.util.*;
 
-public class BOJ_10427 {
-
-	static long[] arr;
-	static long[] s;
-	static int N;
-	static long sum, minVal;
-
-	public static void main(String[] args) throws IOException {
+public class Main{
+	static int T, N;
+	static int[] arr, s;
+	
+	public static void main(String[] args)throws IOException {
+//		System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-
-		for (int t = 0; t < T; t++) {
-
-			st = new StringTokenizer(br.readLine());
+		T = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc < T+1; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
-			arr = new long[N + 1];
-			s = new long[N + 1];
-			sum = 0;
-
-			for (int i = 1; i <= N; i++) {
+			arr = new int[N+1];
+			s = new int[N+1]; s[1] = 0;
+			for (int i = 1; i < N+1; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
 			}
 			Arrays.sort(arr);
-
-			for (int i = 1; i <= N; i++) {
-				if (i == 1) {
-					s[i] = arr[i]; // 초기값 설정
-				} else {
-					s[i] = s[i - 1] + arr[i]; // 누적합 구하기
+			for (int M = 2; M < N+1; M++) {
+				int ttl = 0;
+				for (int i = 1; i < M; i++) {
+					ttl += arr[M] - arr[i];
 				}
-			}
-
-			for (int i = 2; i <= N; i++) {  // i: 뽑는 개수
-				minVal = Long.MAX_VALUE;
-				for (int j = i; j <= N; j++) { 
-					minVal = Math.min(minVal, arr[j] * i - (s[j] - s[j - i]));  // 뽑힌 수 중 가장 큰 수 * i - (뽑힌 수 의 합)
+				int mini = ttl;
+				for (int i = 1; i < N+1-M; i++) {
+					ttl += (arr[i+ M] - arr[i+M-1]) * M;
+					ttl -= arr[i+ M] - arr[i];
+					if (mini > ttl) {
+						mini = ttl;
+					}
 				}
-				sum += minVal;
+				s[M] = mini;
 			}
-			System.out.println(sum);
+			int ttl = 0;
+			for (int i = 1; i < N+1; i++) {
+				ttl += s[i]; 
+			}
+			sb.append(ttl).append("\n");
 		}
+		System.out.println(sb);
 	}
 }
